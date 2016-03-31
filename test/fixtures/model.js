f6c5880cb7467f0dbdd50113dc2a6e7fb5dad927
世外桃源 <ooxfordck@gmail.com>
2016-04-01 01:06:49 +0800

    upup

--- test/fixtures/model.js
+++ test/fixtures/model.js
@@ -1,23 +1,23 @@
 "use strict";
-var P = require( "bluebird" );
-var sinon = require( "sinon" );
+var P = require("bluebird");
+var sinon = require("sinon");
 var resolverStub = sinon.stub();
 var execStub = sinon.stub();
 var findStub = sinon.stub();
 
 module.exports = {
-  find         : findStub,
-  findStub     : findStub,
-  execStub     : execStub,
-  resolverStub : resolverStub,
-  reset        : function(){
+  find: findStub,
+  findStub: findStub,
+  execStub: execStub,
+  resolverStub: resolverStub,
+  reset: function () {
     resolverStub.reset();
     execStub.reset();
-    execStub.returns( new P( resolverStub ) );
+    execStub.returns(new P(resolverStub));
     findStub.reset();
-    findStub.returns( {
-      exec : execStub
-    } );
+    findStub.returns({
+      exec: execStub
+    });
   }
 };
 
--- test/methods/list.spec.js
+++ test/methods/list.spec.js
@@ -3,162 +3,162 @@
 /* global describe, it, beforeEach, afterEach */
 /* jshint unused:false */
 
-var _ = require( "lodash" );
-var sinon = require( "sinon" );
-var stub = require( "proxyquire" ).noCallThru();
-var expect = require( "must" );
-var fx = require( "../fixtures" );
+var _ = require("lodash");
+var sinon = require("sinon");
+var stub = require("proxyquire").noCallThru();
+var expect = require("must");
+var fx = require("../fixtures");
 var stubs, subject;
 
-describe( "lib/methods/list", function(){
-  beforeEach( function(){
+describe("lib/methods/list", function () {
+  beforeEach(function () {
     stubs = {
-      "./retrieve" : sinon.stub()
+      "./retrieve": sinon.stub()
     };
-    subject = stub( "../../lib/methods/list", stubs );
-  } );
+    subject = stub("../../lib/methods/list", stubs);
+  });
 
-  describe( "spec file", function(){
-    it( "should be found", function(){
-      expect( true ).to.be.true();
-    } );
-  } );
+  describe("spec file", function () {
+    it("should be found", function () {
+      expect(true).to.be.true();
+    });
+  });
 
-  describe( "module", function(){
-    it( "should be a function", function(){
-      expect( subject ).to.be.a.function();
-    } );
-    it( "should return a configuration object", function(){
-      var result = subject( {
-        path : "test"
-      } );
-      expect( result ).to.have.keys( [ "handle", "verb", "url" ] );
-    } );
+  describe("module", function () {
+    it("should be a function", function () {
+      expect(subject).to.be.a.function();
+    });
+    it("should return a configuration object", function () {
+      var result = subject({
+        path: "test"
+      });
+      expect(result).to.have.keys(["handle", "verb", "url"]);
+    });
 
-    describe( "#verb", function(){
-      it( "should be 'get'", function(){
-        var result = subject( {
-          path : "test"
-        } );
-        expect( result.verb ).to.equal( "get" );
-      } );
-    } );
+    describe("#verb", function () {
+      it("should be 'get'", function () {
+        var result = subject({
+          path: "test"
+        });
+        expect(result.verb).to.equal("get");
+      });
+    });
 
-    describe( "#url", function(){
-      it( "should be the value of `entry`", function(){
+    describe("#url", function () {
+      it("should be the value of `entry`", function () {
         var entry = Math.random();
-        var result = subject( {
-          path : "test"
-        }, {}, entry );
-        expect( result.url ).to.equal( entry );
-      } );
-    } );
+        var result = subject({
+          path: "test"
+        }, {}, entry);
+        expect(result.url).to.equal(entry);
+      });
+    });
 
-    describe( "#handle", function(){
+    describe("#handle", function () {
       var config, req;
-      beforeEach( function(){
-        config = subject( {
-          path  : "test",
-          model : fx.model
-        } );
+      beforeEach(function () {
+        config = subject({
+          path: "test",
+          model: fx.model
+        });
         req = {
-          params : {},
-          query  : {},
-          body   : {}
+          params: {},
+          query: {},
+          body: {}
         };
-      } );
-      afterEach( function(){
+      });
+      afterEach(function () {
         fx.model.reset();
-      } );
-      it( "should be a function", function(){
-        expect( config.handle ).to.be.a.function();
-      } );
-      it( "should delegate to `retrieve` when a `id` param is present", function(){
+      });
+      it("should be a function", function () {
+        expect(config.handle).to.be.a.function();
+      });
+      it("should delegate to `retrieve` when a `id` param is present", function () {
         var retrieveResult = {
-          handle : sinon.spy()
+          handle: sinon.spy()
         };
-        stubs[ "./retrieve" ].returns( retrieveResult );
+        stubs["./retrieve"].returns(retrieveResult);
         req.params.id = "a valid id";
-        config.handle( req );
-        expect( stubs[ "./retrieve" ].callCount ).to.equal( 1 );
-        expect( retrieveResult.handle.callCount ).to.equal( 1 );
-      } );
-      it( "should use a `filter` param", function(){
+        config.handle(req);
+        expect(stubs["./retrieve"].callCount).to.equal(1);
+        expect(retrieveResult.handle.callCount).to.equal(1);
+      });
+      it("should use a `filter` param", function () {
         var filter = {};
         req.params.filter = filter;
-        config.handle( req );
-        expect( fx.model.findStub.calledWith( filter ) ).to.be.true();
-      } );
-      it( "should translate a `filter` param string to an object", function(){
+        config.handle(req);
+        expect(fx.model.findStub.calledWith(filter)).to.be.true();
+      });
+      it("should translate a `filter` param string to an object", function () {
         var filter = "{}";
         req.params.filter = filter;
-        config.handle( req );
-        expect( fx.model.findStub.args[ 0 ] ).to.eql( [
+        config.handle(req);
+        expect(fx.model.findStub.args[0]).to.eql([
           {}, undefined, {
-            name : "test"
+            name: "test"
           }
-        ] );
-      } );
-      it( "should use `config.filter`", function(){
+        ]);
+      });
+      it("should use `config.filter`", function () {
         var filter = {};
-        config = subject( {
-          path  : "test",
-          model : fx.model
+        config = subject({
+          path: "test",
+          model: fx.model
         }, {
-          filter : filter
-        } );
-        config.handle( req );
-        expect( fx.model.findStub.calledWith( filter ) ).to.be.true();
-      } );
-      it( "should execute `config.filter` when its a function", function(){
+          filter: filter
+        });
+        config.handle(req);
+        expect(fx.model.findStub.calledWith(filter)).to.be.true();
+      });
+      it("should execute `config.filter` when its a function", function () {
         var filter = sinon.spy();
-        config = subject( {
-          path  : "test",
-          model : fx.model
+        config = subject({
+          path: "test",
+          model: fx.model
         }, {
-          filter : filter
-        } );
-        config.handle( req );
-        expect( filter.callCount ).to.equal( 1 );
-      } );
-      it( "should merge `config.filter` and a `filter` param", function(){
+          filter: filter
+        });
+        config.handle(req);
+        expect(filter.callCount).to.equal(1);
+      });
+      it("should merge `config.filter` and a `filter` param", function () {
         var cFilter = {
-          a : "cFilter"
+          a: "cFilter"
         };
         var rFilter = {
-          b : "rFilter"
+          b: "rFilter"
         };
-        config = subject( {
-          path  : "test",
-          model : fx.model
+        config = subject({
+          path: "test",
+          model: fx.model
         }, {
-          filter : cFilter
-        } );
+          filter: cFilter
+        });
         req.query.filter = rFilter;
-        config.handle( req );
-        expect( fx.model.findStub.getCall( 0 ).args[ 0 ] ).to.eql( {
-          a : "cFilter",
-          b : "rFilter"
-        } );
-      } );
-      it( "should call model.find", function(){
+        config.handle(req);
+        expect(fx.model.findStub.getCall(0).args[0]).to.eql({
+          a: "cFilter",
+          b: "rFilter"
+        });
+      });
+      it("should call model.find", function () {
         var opts = {
-          filter : {
-            filtered : true
+          filter: {
+            filtered: true
           },
-          show   : "fields"
+          show: "fields"
         };
-        config = subject( {
-          path  : "test",
-          model : fx.model
-        }, opts );
-        config.handle( req );
-        expect( fx.model.findStub.getCall( 0 ).args ).to.eql( [
-          opts.filter, opts.show, _.defaults( opts, {
-            name : "test"
-          } )
-        ] );
-      } );
-    } );
-  } );
-} );
+        config = subject({
+          path: "test",
+          model: fx.model
+        }, opts);
+        config.handle(req);
+        expect(fx.model.findStub.getCall(0).args).to.eql([
+          opts.filter, opts.show, _.defaults(opts, {
+            name: "test"
+          })
+        ]);
+      });
+    });
+  });
+});
